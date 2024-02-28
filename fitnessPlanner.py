664ccedcaf580f347f47618dd395236f9ab752b9
Anish Bhowmik <anish.bhowmik02@gmail.com>
2024-02-28 18:27:50 -0500

    Created function to calcualte maintenance calories

--- fitnessPlanner.py
+++ fitnessPlanner.py
@@ -4,3 +4,14 @@ def calculateMetabolicRate(weight, height, age, sex):
     else:
         bmr = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)
     return bmr
+
+def calculateMaintenanceCalores(bmr, activity_level):
+    if activity_level == "none":
+        calories = bmr * 1.2
+    elif activity_level == "lightly active":
+        calories = bmr * 1.375
+    elif activity_level == "moderately active":
+        calories = bmr * 1.55
+    else:
+        calories = bmr * 1.725
+    return calories
