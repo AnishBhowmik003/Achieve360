e8c65fb68a4020467850906747fc55091179b294
Anish Bhowmik <anish.bhowmik02@gmail.com>
2024-02-28 18:35:18 -0500

    Added functions for weight loss/gain

--- fitnessPlanner.py
+++ fitnessPlanner.py
@@ -5,13 +5,23 @@ def calculateMetabolicRate(weight, height, age, sex):
         bmr = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)
     return bmr
 
-def calculateMaintenanceCalores(bmr, activity_level):
+def calculateMaintenanceCalories(bmr, activity_level):
     if activity_level == "none":
         calories = bmr * 1.2
     elif activity_level == "lightly active":
         calories = bmr * 1.375
     elif activity_level == "moderately active":
         calories = bmr * 1.55
+    elif activity_level == "very active":
+        calories = bmr * 1.725
+    elif activity_level == "extra active":
+        calories = bmr * 1.9
     else:
         calories = bmr * 1.725
     return calories
+
+def calculateWeightLossCalories(bmr, activity_level):
+    return calculateMaintenanceCalories(bmr, activity_level) - 500
+
+def calculateWeightGainCalories(bmr, activity_level):
+    return calculateMaintenanceCalories(bmr, activity_level) + 500
