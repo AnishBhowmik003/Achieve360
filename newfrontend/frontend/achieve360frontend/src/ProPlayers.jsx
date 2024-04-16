ee4a3c1c46291ed977b0791f0795c4e4043e4384
mkg880 <mehrzadgandhi@gmail.com>
2024-04-16 19:32:50 -0400

    Made pro players output look nice

--- newfrontend/frontend/achieve360frontend/src/ProPlayers.jsx
+++ newfrontend/frontend/achieve360frontend/src/ProPlayers.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ProPlayers = ({ onBackToDashboard, email }) => {
     const [sport, setSport] = useState('');
     const sportOptions = ['Football', 'Basketball', 'Baseball', 'Running'];
-    const [backendOutput, setBackendOutput] = useState('');
     const [submitted, setSubmitted] = useState(false);
-
+    const [displayData, setDisplayData] = useState();
+    const [tableHead, setTableHead] = useState();
+    useEffect(() => {
+      console.log(tableHead);
+   }, [tableHead]);
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -21,12 +24,27 @@ const ProPlayers = ({ onBackToDashboard, email }) => {
           });
           const data = await response.json();
           if (response.ok) {
-            console.log(data.res);
-            var str = "";
-            for(var i = 0; i < data.res.length; i++) {
-                str += JSON.stringify(data.res[i]) + "\n";
-            }
-            setBackendOutput(str);
+            setDisplayData(data.res.map((info) => {
+              return (
+                <tr>
+                  {Object.keys(info).map((value) => {
+                    return (
+                    <td> {typeof info[value] == 'number' && !Number.isInteger(info[value]) ? info[value].toFixed(3) : info[value]}</td>
+                    )
+                  })}
+                </tr>
+              )
+            }));
+            setTableHead((
+                <tr>
+                  {Object.keys(data.res[0]).map((value) => {
+                    return (
+                    <th>{value}</th>
+                    )
+                  })}
+                </tr>
+              )
+            );
             setSubmitted(true);
           } else {
             console.error('Error.');
@@ -39,7 +57,8 @@ const ProPlayers = ({ onBackToDashboard, email }) => {
       const resetForm = () => {
         setSport('');
         setSubmitted(false);
-        setBackendOutput('');
+        setDisplayData();
+        setTableHead();
       };
 
     return (
@@ -47,11 +66,14 @@ const ProPlayers = ({ onBackToDashboard, email }) => {
         {submitted ? (
             <div>
               <h1>Script Output:</h1>
-              <textarea readOnly
-                value={backendOutput}
-                // onChange={(e) => setBackendOutput(e.target.value)}
-                style={{ width: '100%', height: '750px', marginBottom: '10px' }} // Adjust size as needed
-              />
+              <table className="styled-table">
+                <thead>
+                  {tableHead}
+                </thead>
+                <tbody>
+                  {displayData}
+                </tbody>
+              </table>
               <div>
                 <button onClick={onBackToDashboard} style={{ marginRight: '10px', marginBottom: '10px' }}>Back to Dashboard</button>
                 <button onClick={resetForm} style={{ marginBottom: '10px' }}>Select Different Sport</button>
