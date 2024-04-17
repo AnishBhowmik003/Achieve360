cfff3e5781e7278209e4057360670ef2f97e028f
ishaanpathania <ipathani@purdue.edu>
2024-04-17 17:08:55 -0400

    Cleaned basketball data

--- datasets/data_clean.py
+++ datasets/data_clean.py
@@ -4,4 +4,10 @@ data = pd.read_csv('/Users/ishaanpathania/Desktop/Achieve360/datasets/fullNBACom
 
 data.fillna(0, inplace=True)
 
+for i, row in data.iterrows():
+    if row['height'] == 0 or row['weight'] == 0:
+        data.drop(i, inplace=True)
+
+data.reset_index(drop=True, inplace=True)
+
 data.to_csv('/Users/ishaanpathania/Desktop/Achieve360/datasets/cleanedNBACombine.csv', index=False)
