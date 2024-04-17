c6ca59c71191c36d7beda83641542fa5ca31e9c7
ishaanpathania <ipathani@purdue.edu>
2024-04-17 16:56:31 -0400

    Cleaned again

--- datasets/data_clean.py
+++ datasets/data_clean.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 data = pd.read_csv('/Users/ishaanpathania/Desktop/Achieve360/datasets/fullNBACombine.csv')
 
-data.fillna(pd.NA, inplace=True)
+data.fillna(0, inplace=True)
 
 data.to_csv('/Users/ishaanpathania/Desktop/Achieve360/datasets/cleanedNBACombine.csv', index=False)
