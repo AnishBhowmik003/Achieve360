cf6ad49578986fdd3714dce550b79d081150e6e5
ishaanpathania <ipathani@purdue.edu>
2024-04-18 15:24:43 -0400

    Started frontend for display/edit workouts

--- newfrontend/frontend/achieve360frontend/src/seeWorkouts.jsx
+++ newfrontend/frontend/achieve360frontend/src/seeWorkouts.jsx
@@ -2,6 +2,30 @@
 import React from 'react';
 
 const SeeWorkouts = ({ onNavigate }) => {
+  const [time, setTime] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [array, setArray] = useState([]);
+  const [email, setEmail] = useState([]);
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await fetch('http://localhost:6969/getWorkouts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    const data = await response.json();
+    if (response.ok) {
+      console.log(data.res.map(a => a.username));
+  }
+}
+
+const selectDate = async (e) => {
+  e.preventDefault();
+}
+
   return (
     <div className="see-workouts-container">
       <h1>Workout History</h1>
